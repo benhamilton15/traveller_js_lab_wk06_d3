0abd5b0ae7f8aed02294e64fee553e620ef17bf4
Ben Hamilton <ben.hamilton@me.com>
2019-05-09 09:37:38 +0100

    finished extentions

--- models/traveller.js
+++ models/traveller.js
@@ -3,27 +3,40 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  const result = this.journeys.map(journey => journey.startLocation)
+  return result
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  const result = this.journeys.map(journey => journey.endLocation)
+  return result
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  const result = this.journeys.filter(journey => journey.transport === transport)
+  return result
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  const result = this.journeys.filter(journey => journey.distance > minDistance)
+  return result
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const result = this.journeys.reduce((runningTotal, journey) => runningTotal + journey.distance, 0)
+  return result
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  const result = this.journeys.reduce((runningTotal, journey) =>{
+    if (!(runningTotal.includes(journey.transport))) {
+      return runningTotal.concat(journey.transport)
+    }
+    else {
+      return runningTotal
+    }
+  }, []);
+  return result
 };
 
 
